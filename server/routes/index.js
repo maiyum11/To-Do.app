f834fe0411eb8477f7070d530d408d2cbec4f1b4
maiyum11 <mariam.shahnawaz@ontariotechu.net>
2022-11-18 15:26:00 -0500

    11

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,6 @@ let router = express.Router();
 let indexController = require('../controller/index')
 
 /* GET home page. */
-router.get('/', indexController.displayHomePage);
-
-/* GET about me page. */
-router.get('/home',indexController.displayHomePage);
+router.get('/home', indexController.displayHomePage);
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.Router();
 
 /* GET users listing. */
-app.get('/index', function(req, res, next) {
+app.get('/home', function(req, res, next) {
   res.send('respond with a resource');
 });
 
