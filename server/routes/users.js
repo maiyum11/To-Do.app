6c879d30ba1a7802d8735546aa932a8da78b8075
maiyum11 <mariam.shahnawaz@ontariotechu.net>
2022-11-18 15:41:57 -0500

    14

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 /* GET users listing. */
-router.get('/tasks', function(req, res, next) {
+router.get('/index', function(req, res, next) {
   res.send('respond with a resource');
 });
 
